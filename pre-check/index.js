e6bb1c9be1bb852b27c502a9c1197673275de110
dongxingyan <dongxingyan3@jd.com>
2020-08-01 20:10:56 +0800

    feat:脚本完善

---
+++ pre-check/index.js
@@ -0,0 +1,51 @@
+/*
+ * @Author: created by dongxingyan
+ * @Date: 2020-05-26 22:16:27
+ */ 
+'use strict';
+// nodejs 异步流程控制模块co
+const co = require('co');
+const extend = require('xtend');
+const ora = require('ora');
+const getUnusedPackages = require('./get-unused-packages');
+const createPackageSummary = require('./create-package-summary');
+
+module.exports = function (currentState) {
+    return co(function *() {
+        yield getUnusedPackages(currentState);
+
+        const spinner = ora(`Checking npm registries for updated packages.`);
+        spinner.enabled = spinner.enabled && currentState.get('spinner');
+        spinner.start();
+
+        const cwdPackageJson = currentState.get('cwdPackageJson');
+
+        function dependencies(pkg) {
+            if (currentState.get('global')) {
+                return currentState.get('globalPackages');
+            }
+
+            if (currentState.get('ignoreDev')) {
+                return pkg.dependencies;
+            }
+
+            if (currentState.get('devOnly')) {
+                return pkg.devDependencies;
+            }
+
+            return extend(pkg.dependencies, pkg.devDependencies);
+        }
+
+        const allDependencies = dependencies(cwdPackageJson);
+        const allDependenciesIncludingMissing = Object.keys(extend(allDependencies, currentState.get('missingFromPackageJson')));
+
+        const arrayOfPackageInfo = yield allDependenciesIncludingMissing
+            .map(moduleName => createPackageSummary(moduleName, currentState))
+            .filter(Boolean);
+
+        currentState.set('packages', arrayOfPackageInfo);
+
+        spinner.stop();
+        return currentState;
+    });
+};
---
+++ test.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: created by dongxingyan
+ * @Date: 2020-05-26 23:03:38
+ */ 
+const aaa = require('./pre-check-dependence');
+aaa({
+    checkPackageName:['@ltfe/ltfe-core-lib'],
+        install: true,
+        verbose: true,
+    }
+)
